482436aa2889946f276e60bde79a3e45274698d6
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-12-02 00:38:37 +1100

    update find doctor page

--- stay-healthy/src/Components/InstantConsultationBooking/FindDoctorSearch/FindDoctorSearch.jsx
+++ stay-healthy/src/Components/InstantConsultationBooking/FindDoctorSearch/FindDoctorSearch.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import findDoctor from "./12291154_Wavy_Tech-25_Single-08.svg";
 
 const initSpeciality = [
     'Dentist', 'Gynecologist/obstetrician', 'General Physician', 'Dermatologist', 'Ear-nose-throat (ent) Specialist', 'Homeopath', 'Ayurveda'
@@ -31,7 +32,55 @@ const FindDoctorSearch = () => {
                         </div>)
                     }
                 </div>
+                <div className='mx-auto my-3 d-flex justify-content-center w-75' >
+                    <img className='w-50 ' src={findDoctor} alt="" hidden={!listDoctorsHidden}/>
+                </div>
+                <h4 className='text-center mb-0 pb-0'> 8 doctors available</h4>
+                <h5 className='text-center opacity-50 mt-0 pt-0'> Book appointments with minimum wait time and verified doctor details</h5>
+                <hr className='w-75 mx-auto mt-0 pt-0'/>
+
                 
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
 
 
 
