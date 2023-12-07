0777c9610e17482c73b2ca405b6d0a4bba9b309a
CeeObi <chukwudimaobi@gmail.com>
2023-12-06 23:31:30 -0500

    update appointment details

--- src/Components/Notification/Notification.jsx
+++ src/Components/Notification/Notification.jsx
@@ -37,26 +37,31 @@ const Notification = ({ children }) => {
       {children}
       {isLoggedIn && appointmentData && (
         <>
-        <h3 className="text-center">Appointment Details</h3>
-          <div className="card col-3 mx-auto rounded-0">            
-          <p className="card-header text-center fs-5">
+        <hr className='mt-2 pt-0 col-10 mx-auto'/>
+        <h3 className="text-center mt-0">Appointment Details</h3>
+          <div className="mb-0 card col-2 mx-2 rounded shadow mh-25 bg-info">            
+            <div className='border border-primary m-1 rounded'>
+                <p className="card-header text-center fs-6">
                     <strong>Patient Name: </strong> {doctorData?.name}              
                 </p>
-            <div className="card-body mx-auto">
-                <p className="card-group">
-                    <strong>Patient Mobile:</strong> {doctorData?.phone}             
-                </p>
-                <p className="card-group">
-                    <strong>Date of Appointment:</strong> {doctorData?.date}                
-                </p>
-                <p className="card-group">
-                    <strong>Time:</strong> {doctorData?.time}            
-                </p>
-            </div>
-            <div className='w-100 mx-auto'>
-            <p className='mb-0 pb-0 mx-3 text-decoration-underline text-primary text-center'> Physician Details:</p>
-            <DrCard key={doctorData?.id} pixsrc={doctorData?.drpix} drname={doctorData?.drname} specialty={doctorData?.specialty} noexp={doctorData?.experience} ratings={doctorData?.ratings}/>
-            </div>
+                <div className="card-body mx-auto mt-1">
+                    <p className="card-group my-0 pt-1 fs-6">
+                        <strong>Patient Mobile:</strong> {doctorData?.phone}             
+                    </p>
+                    <p className="card-group my-0 pt-1 fs-6">
+                        <strong>Date of Appointment:</strong> {doctorData?.date}                
+                    </p>
+                    <p className="card-group my-0 pt-1 fs-6">
+                        <strong>Time:</strong> {doctorData?.time}            
+                    </p>
+                    <p className="card-group my-0 pt-1 fs-6">
+                        <strong>Doctor Name:</strong> {doctorData?.drname}            
+                    </p>
+                    <p className="card-group my-0 pt-1 fs-6">
+                        <strong>Specialty:</strong> {doctorData?.specialty}            
+                    </p>
+                </div>
+                </div>
           </div>
           
         </>
